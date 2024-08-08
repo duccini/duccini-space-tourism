d902a8dd90a94fef82c3446cc0ef7453a75e5da3
duccini <guilhermeduccini@gmail.com>
2024-08-07 22:23:47 -0300

    finish homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Menu from "./_components/Menu";
 import styles from "./page.module.scss";
 
@@ -7,8 +8,23 @@ export default function Home() {
       <Menu />
 
       <main>
-        <h1>SPACE</h1>
-        <p>Teste teste teste</p>
+        <div className={styles.homehero}>
+          <div className={styles.homeheroContent}>
+            <h2>SO, YOU WANT TO TRAVEL TO</h2>
+            <h1>SPACE</h1>
+            <p>
+              Let’s face it; if you want to go to space, you might as well
+              genuinely go to outer space and not hover kind of on the edge of
+              it. Well sit back, and relax because we’ll give you a truly out of
+              this world experience!
+            </p>
+          </div>
+          <div className={styles.homeheroAction}>
+            <Link href="/destinations">
+              <div className={styles.oval}>Explore</div>
+            </Link>
+          </div>
+        </div>
       </main>
     </div>
   );
